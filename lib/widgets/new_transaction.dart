307b9e2db0e86abb3b57f9e206fabcd21e792958
Salz <faisal_animator@yahoo.com>
2024-04-07 10:36:21 +0700

    feat: Add Step 8

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final Function addTx;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+              // onChanged: (val) {
+              //   titleInput = val;
+              // },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+              // onChanged: (val) => amountInput = val,
+            ),
+            ElevatedButton(
+              child: Text('Add Transaction'),
+              style: ElevatedButton.styleFrom(
+                textStyle: TextStyle(color: Colors.purple),
+                fixedSize: Size.fromWidth(100),
+                padding: EdgeInsets.all(10),
+              ),
+              onPressed: () {
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
